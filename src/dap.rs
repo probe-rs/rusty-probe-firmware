ea9fbe283053917673a573991179fe29723357f1
Emil Fresk <emil.fresk@gmail.com>
2022-02-16 15:53:01 +0100

    First impl done

--- src/dap.rs
+++ src/dap.rs
@@ -1,46 +1,129 @@
 use dap_rs::{swj::Swj, *};
 use embedded_hal::{
     blocking::delay::DelayUs,
-    digital::v2::{InputPin, OutputPin},
+    digital::v2::{InputPin, OutputPin, PinState},
 };
+use rp_pico::hal::gpio::DynPin;
 
-pub trait InputToOutput<PO: OutputPin> {
-    fn to_output(self) -> PO;
-}
-
-pub trait OutputToInput<PI: InputPin> {
-    fn to_input(self) -> PI;
-}
 pub struct Context {
     max_frequency: u32,
     cpu_frequency: u32,
+    cycles_per_us: u32,
+    half_period_ticks: u32,
+    swdio: DynPin,
+    swclk: DynPin,
+    nreset: DynPin,
 }
 
 impl dap::DapContext for Context {
     fn high_impedance_mode(&mut self) {
-        todo!()
+        self.swdio.into_floating_disabled();
+        self.swclk.into_floating_disabled();
+        self.nreset.into_floating_disabled();
     }
 }
 
 impl Context {
-    pub fn from_pins() -> Self {
-        todo!()
+    pub fn from_pins(swdio: DynPin, swclk: DynPin, nreset: DynPin, cpu_frequency: u32) -> Self {
+        let max_frequency = 100_000;
+        let half_period_ticks = cpu_frequency / max_frequency / 2;
+        Context {
+            max_frequency,
+            cpu_frequency,
+            cycles_per_us: cpu_frequency / 1_000_000,
+            half_period_ticks,
+            swdio,
+            swclk,
+            nreset,
+        }
     }
 }
 
 impl swj::Swj for Context {
-    fn pins(&mut self, output: swj::Pins, mask: swj::Pins, wait_us: u32) -> u8 {
-        todo!()
+    fn pins(&mut self, output: swj::Pins, mask: swj::Pins, wait_us: u32) -> swj::Pins {
+        if mask.contains(swj::Pins::SWCLK) {
+            self.swclk.into_push_pull_output();
+            self.swclk
+                .set_state(if output.contains(swj::Pins::SWCLK) {
+                    PinState::High
+                } else {
+                    PinState::Low
+                })
+                .ok();
+        }
+
+        if mask.contains(swj::Pins::SWDIO) {
+            self.swdio.into_push_pull_output();
+            self.swdio
+                .set_state(if output.contains(swj::Pins::SWDIO) {
+                    PinState::High
+                } else {
+                    PinState::Low
+                })
+                .ok();
+        }
+
+        if mask.contains(swj::Pins::NRESET) {
+            if output.contains(swj::Pins::NRESET) {
+                // "open drain"
+                self.nreset.into_floating_disabled();
+            } else {
+                self.nreset.into_push_pull_output();
+                self.nreset.set_low().ok();
+            }
+        }
+
+        cortex_m::asm::delay(self.cycles_per_us * wait_us);
+
+        self.swclk.into_floating_input();
+        self.swdio.into_floating_input();
+        self.nreset.into_floating_input();
+
+        let mut ret = swj::Pins::empty();
+        ret.set(swj::Pins::SWCLK, matches!(self.swclk.is_high(), Ok(true)));
+        ret.set(swj::Pins::SWDIO, matches!(self.swdio.is_high(), Ok(true)));
+        ret.set(swj::Pins::NRESET, matches!(self.nreset.is_high(), Ok(true)));
+
+        ret
     }
 
-    fn sequence(&mut self, data: &[u8], nbits: usize) {
-        todo!()
+    fn sequence(&mut self, data: &[u8], mut bits: usize) {
+        self.swdio.into_push_pull_output();
+        self.swclk.into_push_pull_output();
+
+        let half_period_ticks = self.half_period_ticks;
+
+        for byte in data {
+            let mut byte = *byte;
+            let frame_bits = core::cmp::min(bits, 8);
+            for _ in 0..frame_bits {
+                let bit = byte & 1;
+                byte >>= 1;
+                if bit != 0 {
+                    self.swdio.set_high().ok();
+                } else {
+                    self.swdio.set_low().ok();
+                }
+                self.swclk.set_low().ok();
+                cortex_m::asm::delay(half_period_ticks);
+                self.swclk.set_high().ok();
+                cortex_m::asm::delay(half_period_ticks);
+            }
+            bits -= frame_bits;
+        }
+
+        self.swclk.into_floating_input();
+        self.swdio.into_floating_input();
     }
 
     fn set_clock(&mut self, max_frequency: u32) -> bool {
-        self.max_frequency = max_frequency;
-
-        true
+        if max_frequency < self.cpu_frequency {
+            self.max_frequency = max_frequency;
+            self.half_period_ticks = self.cpu_frequency / self.max_frequency / 2;
+            true
+        } else {
+            false
+        }
     }
 }
 
@@ -77,32 +160,189 @@ pub struct Swd(Context);
 impl swd::Swd<Context> for Swd {
     const AVAILABLE: bool = true;
 
-    fn new(context: Context) -> Self {
-        todo!()
+    fn new(mut context: Context) -> Self {
+        context.swdio.into_push_pull_output();
+        context.swclk.into_push_pull_output();
+
+        Self(context)
     }
 
-    fn release(self) -> Context {
-        todo!()
+    fn release(mut self) -> Context {
+        self.0.swclk.into_floating_input();
+        self.0.swdio.into_floating_input();
+
+        self.0
     }
 
     fn configure(&mut self, period: swd::TurnaroundPeriod, data_phase: swd::DataPhase) -> bool {
-        todo!()
+        period == swd::TurnaroundPeriod::Cycles1 && data_phase == swd::DataPhase::NoDataPhase
     }
 
     fn read_inner(&mut self, apndp: swd::APnDP, a: swd::DPRegister) -> swd::Result<u32> {
-        todo!()
+        // Send request
+        let req = swd::make_request(apndp, swd::RnW::R, a);
+        self.tx8(req);
+
+        // Read ack, 1 clock for turnaround and 3 for ACK
+        let ack = self.rx4() >> 1;
+        match swd::Ack::try_ok(ack as u8) {
+            Ok(_) => (),
+            Err(e) => {
+                // On non-OK ACK, target has released the bus but
+                // is still expecting a turnaround clock before
+                // the next request, and we need to take over the bus.
+                self.idle_low();
+                return Err(e);
+            }
+        }
+
+        // Read data and parity
+        let (data, parity) = self.read_data();
+
+        // Turnaround + trailing
+        self.rx8();
+        self.write_bit(0); // Drive the SWDIO line to 0 to not float
+
+        if parity as u8 == (data.count_ones() as u8 & 1) {
+            Ok(data)
+        } else {
+            Err(swd::Error::BadParity)
+        }
     }
 
     fn write_inner(&mut self, apndp: swd::APnDP, a: swd::DPRegister, data: u32) -> swd::Result<()> {
-        todo!()
-    }
+        // Send request
+        let req = swd::make_request(apndp, swd::RnW::W, a);
+        self.tx8(req);
 
-    fn idle_low(&mut self) {
-        todo!()
+        // Read ack, 1 clock for turnaround and 3 for ACK and 1 for turnaround
+        let ack = (self.rx5() >> 1) & 0b111;
+        match swd::Ack::try_ok(ack as u8) {
+            Ok(_) => (),
+            Err(e) => {
+                // On non-OK ACK, target has released the bus but
+                // is still expecting a turnaround clock before
+                // the next request, and we need to take over the bus.
+                self.idle_low();
+                return Err(e);
+            }
+        }
+
+        // Send data and parity
+        let parity = data.count_ones() & 1 == 1;
+        self.send_data(data, parity);
+
+        // Send trailing idle
+        self.tx8(0);
+
+        Ok(())
     }
 
     fn set_clock(&mut self, max_frequency: u32) -> bool {
         self.0.set_clock(max_frequency)
+    }
+}
+
+impl Swd {
+    fn idle_low(&mut self) {
+        for _ in 0..4 {
+            self.write_bit(0);
+        }
+    }
+
+    fn tx8(&mut self, mut data: u8) {
+        self.0.swdio.into_push_pull_output();
+        for _ in 0..8 {
+            self.write_bit(data & 1);
+            data >>= 1;
+        }
+    }
+
+    fn rx4(&mut self) -> u8 {
+        self.0.swdio.into_floating_input();
+
+        let mut data = 0;
+
+        for _ in 0..4 {
+            data |= self.read_bit() & 1;
+            data <<= 1;
+        }
+
+        data
+    }
+
+    fn rx5(&mut self) -> u8 {
+        self.0.swdio.into_floating_input();
+
+        let mut data = 0;
+
+        for _ in 0..5 {
+            data |= self.read_bit() & 1;
+            data <<= 1;
+        }
+
+        data
+    }
+
+    fn rx8(&mut self) -> u8 {
+        self.0.swdio.into_floating_input();
+
+        let mut data = 0;
+
+        for _ in 0..8 {
+            data |= self.read_bit() & 1;
+            data <<= 1;
+        }
+
+        data
+    }
+
+    fn send_data(&mut self, mut data: u32, parity: bool) {
+        self.0.swdio.into_push_pull_output();
+
+        for _ in 0..32 {
+            self.write_bit((data & 1) as u8);
+            data >>= 1;
+        }
+
+        self.write_bit(parity as u8);
+    }
+
+    fn read_data(&mut self) -> (u32, bool) {
+        self.0.swdio.into_floating_input();
+
+        let mut data = 0;
+
+        for _ in 0..32 {
+            data |= (self.read_bit() & 1) as u32;
+            data <<= 1;
+        }
+
+        let parity = self.read_bit() != 0;
+
+        (data, parity)
+    }
+
+    fn write_bit(&mut self, bit: u8) {
+        if bit != 0 {
+            self.0.swdio.set_high().ok();
+        } else {
+            self.0.swdio.set_low().ok();
+        }
+        self.0.swclk.set_low().ok();
+        cortex_m::asm::delay(self.0.half_period_ticks);
+        self.0.swclk.set_high().ok();
+        cortex_m::asm::delay(self.0.half_period_ticks);
+    }
+
+    fn read_bit(&mut self) -> u8 {
+        self.0.swclk.set_low().ok();
+        cortex_m::asm::delay(self.0.half_period_ticks);
+        let bit = matches!(self.0.swdio.is_high(), Ok(true)) as u8;
+        self.0.swclk.set_high().ok();
+        cortex_m::asm::delay(self.0.half_period_ticks);
+
+        bit
     }
 }
 
@@ -154,11 +394,21 @@ impl swo::Swo for Swo {
     }
 }
 
-pub struct Wait {}
+pub struct Wait {
+    cycles_per_us: u32,
+}
+
+impl Wait {
+    pub fn new(cpu_frequency: u32) -> Self {
+        Wait {
+            cycles_per_us: cpu_frequency / 1_000_000,
+        }
+    }
+}
 
 impl DelayUs<u32> for Wait {
     fn delay_us(&mut self, us: u32) {
-        todo!()
+        cortex_m::asm::delay(self.cycles_per_us * us);
     }
 }
 
