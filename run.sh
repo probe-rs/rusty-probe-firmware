135e0098f5a3e4e39f24333ee03d4b83bb3f180f
datdenkikniet <jcdra1@gmail.com>
2023-04-08 19:24:37 +0200

    Check that some commands exist

--- run.sh
+++ run.sh
@@ -3,6 +3,16 @@
 serial=${LOG_SERIAL-"/dev/ttyACM0"}
 file=${1-"./target/thumbv6m-none-eabi/release/app"}
 
+check_exists() {
+    if [ ! -f $(which "$1") ]; then
+        echo "Could not find $1. Please install it. (probably \`cargo install \"$1\"\`)"
+        exit 1
+    fi
+}
+
+check_exists elf2uf2-rs
+check_exists defmt-print
+
 ./restart.sh "$serial"
 elf2uf2-rs -d "$file"
 
